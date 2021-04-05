bacc721b3fa0302054906e801b84258dab5820ad
liberatos278 <72798747+liberatos278@users.noreply.github.com>
2021-04-05 21:50:49 +0200

    feat(messaging): Add new types of messages

--- popup/popup.js
+++ popup/popup.js
@@ -2,11 +2,15 @@ window.onload = async function () {
     const enableButton = document.getElementById('enableCheckbox');
     const variableAmountButton = document.getElementById('variableAmountCheckbox');
     const moveAmountButton = document.getElementById('moveAmountCheckbox');
+    const jailAbleButton = document.getElementById('jailAbleCheckbox');
+    const pointAbleButton = document.getElementById('pointAbleCheckbox');
 
-    chrome.storage.local.get(['enableExtension', 'variableAmount', 'moveAmount'], (result) => {
+    chrome.storage.local.get(['enableExtension', 'variableAmount', 'moveAmount', 'jailAble', 'pointAble'], (result) => {
         variableAmountButton.checked = result.variableAmount ?? true;
         enableButton.checked = result.enableExtension ?? true;
         moveAmountButton.checked = result.moveAmount ?? false;
+        jailAbleButton.checked = result.jailAble ?? true;
+        pointAbleButton.checked = result.pointAble ?? false;
 
         enableButton.addEventListener('change', function () {
 
@@ -36,6 +40,26 @@ window.onload = async function () {
             } else {
                 chrome.storage.local.set({ 'moveAmount': false });
                 sendChange('moveAmount-disabled');
+            }
+        });
+
+        jailAbleButton.addEventListener('change', function () {
+            if(this.checked) {
+                chrome.storage.local.set({ 'jailAble': true });
+                sendChange('jailAble-enabled');
+            } else {
+                chrome.storage.local.set({ 'jailAble': false });
+                sendChange('jailAble-disabled');
+            }
+        });
+
+        pointAbleButton.addEventListener('change', function () {
+            if(this.checked) {
+                chrome.storage.local.set({ 'pointAble': true });
+                sendChange('pointAble-enabled');
+            } else {
+                chrome.storage.local.set({ 'pointAble': false });
+                sendChange('pointAble-disabled');
             }
         });
     });
