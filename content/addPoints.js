cfcb4b2627177e552a27adbb6cf5f219ddec054a
liberatos278 <72798747+liberatos278@users.noreply.github.com>
2021-04-09 10:01:41 +0200

    fix(fine): Fine with points

--- content/addPoints.js
+++ content/addPoints.js
@@ -3,53 +3,55 @@ function addPoints() {
     const pointsToRemove = Number(input.value);
 
     if (!input.value || input.value === '0' || input.value === '') {
-        if(researchFine === true) { searchCitizen(searchedCitizenID); }
+        if (researchFine === true) { searchCitizen(searchedCitizenID); }
         return;
     }
 
-    //DMV site
-    const veh = document.querySelectorAll('[routerlink="/search_vehicles"]')[0];
-    veh.click();
-
     setTimeout(function () {
-        
-        const citizenID = document.querySelectorAll('[placeholder="Zadejte jméno / Citizen ID..."]')[0];
-        const submitBtn = document.getElementsByTagName('button')[1];
 
-        citizenID.value = searchedCitizenID;
-        citizenID.dispatchEvent(new Event('input'));
-        submitBtn.click();
+        //DMV site
+        const veh = document.querySelectorAll('[routerlink="/search_vehicles"]')[0];
+        veh.click();
 
         setTimeout(function () {
-            const firstID = document.getElementsByClassName('incident')[0];
-            firstID.click();
+
+            const citizenID = document.querySelectorAll('[placeholder="Zadejte jméno / Citizen ID..."]')[0];
+            const submitBtn = document.getElementsByTagName('button')[1];
+
+            citizenID.value = searchedCitizenID;
+            citizenID.dispatchEvent(new Event('input'));
+            submitBtn.click();
 
             setTimeout(function () {
-                const actualPoints = Number(document.getElementsByClassName('licensePoints')[1].innerHTML.replace(' / 12', ''));
-                const editPointsBtn = [...document.getElementsByClassName('btn')].find(btn => btn.innerHTML.includes('SPRAVOVAT BODY'));
-
-                showActualPoints(actualPoints-pointsToRemove);
-
-                editPointsBtn.click();
+                const firstID = document.getElementsByClassName('incident')[0];
+                firstID.click();
 
                 setTimeout(function () {
-                    let ptnMinus = [...document.getElementsByClassName('pointsBtn')][1];
+                    const actualPoints = Number(document.getElementsByClassName('licensePoints')[1].innerHTML.replace(' / 12', ''));
+                    const editPointsBtn = [...document.getElementsByClassName('btn')].find(btn => btn.innerHTML.includes('SPRAVOVAT BODY'));
 
-                    for(let i = 0; i < pointsToRemove+(12-actualPoints); i++) {
-                        ptnMinus.click();
-                    }
-
-                    let submit = [...document.getElementsByClassName('btn')];
-                    submit = submit.find(btn => btn.innerHTML.includes('POTVRDIT'));
-
-                    submit.click();
+                    editPointsBtn.click();
 
                     setTimeout(function () {
-                        const crim = document.querySelectorAll('[routerlink="/search_criminals"]')[0];
-                        crim.click();
+                        let ptnMinus = [...document.getElementsByClassName('pointsBtn')][1];
+
+                        for (let i = 0; i < pointsToRemove + (12 - actualPoints); i++) {
+                            ptnMinus.click();
+                        }
+
+                        let submit = [...document.getElementsByClassName('btn')];
+                        submit = submit.find(btn => btn.innerHTML.includes('POTVRDIT'));
+
+                        submit.click();
 
                         setTimeout(function () {
-                            searchCitizen(searchedCitizenID);
+                            const crim = document.querySelectorAll('[routerlink="/search_criminals"]')[0];
+                            crim.click();
+
+                            setTimeout(function () {
+                                searchCitizen(searchedCitizenID);
+                                showActualPoints(actualPoints - pointsToRemove);
+                            }, 350);
                         }, 350);
                     }, 350);
                 }, 350);
@@ -61,12 +63,12 @@ function addPoints() {
 function showActualPoints(ptn) {
     const resultName = document.getElementsByClassName('resultQuery')[0].innerHTML.split(' | ')[0];
     let dlAlert = '';
-    
-    if(ptn < 0) {
+
+    if (ptn < 0) {
         ptn = 0;
         dlAlert = "It is possible to suspend the driver's license";
     }
-    
+
     const finalPoints = `${ptn} / 12`;
     alert(`Driver ${resultName} currently has ${finalPoints} points.\n${dlAlert}`);
 }
