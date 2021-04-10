0219b3dd5f8074b5caa4ff4815753c07263b8638
liberatos278 <72798747+liberatos278@users.noreply.github.com>
2021-04-10 15:09:31 +0200

    fix(betterFine): Loading error handling

--- content/betterFine.js
+++ content/betterFine.js
@@ -5,22 +5,36 @@ function getCitizenID() {
 }
 
 function searchCitizen(id) {
+    let forceReturn = 0;
 
     setTimeout(function () {
         const searchInput = document.getElementById('name');
         const submitBtn = [...document.getElementsByTagName('button')].find(btn => btn.innerText === 'VYHLEDAT');
 
-        if (!searchInput || !submitBtn || !searchedCitizenID) return;
+        
+        if (!searchInput || !submitBtn || !searchedCitizenID) { 
+            forceReturn++;
+            
+            if(forceReturn > 3) return notify('Police Helper', 'The system could not continue searching', '#f02929', 'times', 8000);
+            return searchCitizen(id); 
+        }
 
+        forceReturn = 0;
         searchInput.value = id;
         searchInput.dispatchEvent(new Event('input'));
 
         submitBtn.click();
 
-        setTimeout(function () {
+        setTimeout(function getFirstincident() {
             const firstResult = document.getElementsByClassName('incident')[0];
-            if (!firstResult) return
 
+            if (!firstResult) { 
+                forceReturn++; 
+                
+                if(forceReturn > 3) return notify('Police Helper', 'The system could not continue searching', '#f02929', 'times', 8000);
+                return setTimeout(getFirstincident, 1000); 
+            }
+    
             firstResult.click();
         }, 300);
     }, 500);
