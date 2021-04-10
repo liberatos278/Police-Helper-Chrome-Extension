5fa8f141613c8c6741839631cce0874404d79b30
liberatos278 <72798747+liberatos278@users.noreply.github.com>
2021-04-10 22:34:21 +0200

    feat(suspendedDL): Added new alert after fine

--- content/addPoints.js
+++ content/addPoints.js
@@ -14,7 +14,7 @@ function addPoints() {
         const veh = document.querySelectorAll('[routerlink="/search_vehicles"]')[0];
         const reasonInput = document.getElementById('reason');
 
-        if (reasonInput) {
+        if (!reasonInput) {
             forceReturn++;
             if (forceReturn > 3) return notify('Police Helper', 'The system could not continue', '#f02929', 'times', 8000);
 
@@ -43,14 +43,25 @@ function addPoints() {
             citizenID.dispatchEvent(new Event('input'));
             submitBtn.click();
 
-            setTimeout(function () {
+            setTimeout(function getFirstIncident() {
                 const firstID = document.getElementsByClassName('incident')[0];
+
+                if (!firstID) {
+                    forceReturn++;
+                    if (forceReturn > 3) return notify('Police Helper', 'The system could not continue', '#f02929', 'times', 8000);
+
+                    return setTimeout(getFirstIncident(), 1000);
+                }
+
+                forceReturn = 0;
+
                 firstID.click();
 
                 setTimeout(function specificCitizenVeh() {
                     let actualPoints = document.getElementsByClassName('licensePoints')[1];
                     let editPointsBtn = [...document.getElementsByClassName('btn')].find(btn => btn.innerHTML.includes('SPRAVOVAT BODY'));
                     let resultName = document.getElementsByClassName('resultQuery')[0];
+                    let suspended = [...document.getElementsByTagName('span')].find(span => span.innerHTML.includes('Active') || span.innerHTML.includes('Suspend'));
 
                     if (!actualPoints || !editPointsBtn || !resultName) {
                         forceReturn++;
@@ -102,6 +113,7 @@ function addPoints() {
                             setTimeout(function () {
                                 searchCitizen(searchedCitizenID);
                                 showActualPoints(actualPoints - pointsToRemove, resultName);
+                                if(!suspended.innerHTML === 'Active') notify('Police Helper', `Driver ${resultName} has a suspended driver's license`, '#fc8c03', 'exclamation-triangle', 8000);
                             }, 350);
                         }, 350);
                     }, 350);
